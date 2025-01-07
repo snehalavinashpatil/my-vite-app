3a172d4dc257a07cb0278882f1526a951b2ed70a
snehalavinashpatil <snehal141194@gmail.com>
2025-01-07 14:44:43 -0500

    profile error

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,14 @@
-import EditProfile from './component/EditProfile';
-import { useSelector } from "react-redux";
+// import EditProfile from './component/EditProfile';
+// import { useSelector } from "react-redux";
 
 const Profile = ()=>{
-    const user = useSelector(store=>store.user);
-    return user && (
-        <div>
-        <div>Profile Page</div>
-        <EditProfile user={user}/>
-        </div>
-    )
+    //const user = useSelector(store=>store.user);
+    return   <div>Profile Page</div>
+    // user && (
+    //     <div>
+      //  <div>Profile Page</div>
+    //     <EditProfile user={user}/>
+    //     </div>
+    // )
     }
     export default Profile;
